0bd9196ac6ba6e89270e5a832b33d8cba2572c6a
tuananh582 <phamtuananh582@gmail.com>
2024-07-09 13:47:57 +0700

    first commit

--- testhu/app/src/main/java/com/example/test/uneti.java
+++ testhu/app/src/main/java/com/example/test/uneti.java
@@ -1,6 +1,12 @@
 package com.example.test;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +26,26 @@ public class uneti extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        EditText user = findViewById(R.id.user);
+        EditText pass = findViewById(R.id.password);
+        Button btnsubmit = findViewById(R.id.btnsubmit);
+        btnsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = user.getText().toString();
+                String password= pass.getText().toString();
+                if(TextUtils.isEmpty(username)|TextUtils.isEmpty(password)){
+                    Toast.makeText(uneti.this, "Nhap Day Du Thong Tin", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(uneti.this, "Success", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        
+
+
+
     }
+    //Dang Nhap
+
 }
